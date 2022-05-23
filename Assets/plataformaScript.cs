bc520a5e62bb98ee21eef46faea68b95e14f5c76
gastonedel <102158957+gastonedel@users.noreply.github.com>
2022-05-23 10:27:04 -0300

    cycle

---
+++ Assets/plataformaScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plataformaScript : MonoBehaviour {
+    public float movementSpeed;
+    public float limitxPositive;
+    public bool toRight;
+    public float limitxNegative;
+
+	// Use this for initialization
+	void Start () {
+        toRight = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(toRight==true)
+        {
+            transform.position += new Vector3(movementSpeed, 0, 0);
+
+        }
+        else
+        {
+            transform.position -= new Vector3(movementSpeed, 0, 0);
+        }
+
+        if (transform.position.x > limitxPositive)
+        {
+            toRight = false;
+        }
+        else if(transform.position.x<limitxNegative)
+        {
+            toRight = true;
+        }
+	}
+}
